8675811b532c7f0330b66495cea1ea76f0f73a44
MiraiMurillo <murillomacielec@gmail.com>
2021-10-02 17:16:23 -0300

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import {HomeComponent} from './views/home/home.component';
+import {ProductComponent} from'./views/product/product.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+
+const routes: Routes = [{
+
+  path: "",
+  component: HomeComponent
+  },
+
+  {
+  path:"product",
+  component: ProductComponent
+  },
+
+  {
+    path:"product/create",
+    component: ProductCreateComponent
+  },
+
+  {
+  path:"product/update/:id",
+  component: ProductUpdateComponent
+},
+
+{
+  path:"product/delete/:id",
+  component: ProductDeleteComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
