a3b236e3d6ee21a3cfa44cad271221894d8579d4
Allan <allanvenicio@outlook.com>
2021-10-03 13:16:09 -0300

    criada pasta cliente

---
+++ src/app/views/cliente/cliente.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
+import { HeaderService } from 'src/app/components/templates/header/header.service';
+
+@Component({
+  selector: 'app-cliente',
+  templateUrl: './cliente.component.html',
+  styleUrls: ['./cliente.component.scss']
+})
+export class ClienteComponent implements OnInit {
+
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: "Cadastro de Clientes",
+      icon: 'storefront',
+      routerUrl: '/cliente'
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+    navigateToClienteCreate(): void{
+        this.router.navigate(['cliente/create'])
+    }
+}
