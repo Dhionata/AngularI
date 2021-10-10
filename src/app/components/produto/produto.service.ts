4b51920e0a74ba04243c26254119f82a9dfd5f82
Dhionatã <xiyunagi@gmail.com>
2021-10-10 15:19:02 -0300

    comunicação com backend "has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource."

--- src/app/components/produto/produto.service.ts
+++ src/app/components/produto/produto.service.ts
@@ -2,7 +2,7 @@ import { catchError, map } from 'rxjs/operators';
 import { Produto } from './produto.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { EMPTY, Observable } from 'rxjs';
 
 @Injectable({
@@ -10,24 +10,24 @@ import { EMPTY, Observable } from 'rxjs';
 })
 export class ProdutoService {
 
-  baseUrl = "http://localhost:8080"
+  baseUrl = "http://localhost:8080/produto"
 
   constructor(private snackBar: MatSnackBar,
     private http: HttpClient) { }
 
-    ShowOMessage(msg: string, isError: boolean = false): void {
-      this.snackBar.open(msg, 'X', {
-        duration: 3000,
-        horizontalPosition: "right",
-        verticalPosition: "top",
-        panelClass: isError?['msg-error'] : ['msg-sucess']
-      })
+  ShowOMessage(msg: string, isError: boolean = false): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top",
+      panelClass: isError ? ['msg-error'] : ['msg-sucess']
+    })
   }
 
-  create(produto: Produto): Observable<Produto>{
-    return this.http.post<Produto>(this.baseUrl, produto).pipe(
-    map(obj => obj),
-    catchError(e => this.errorHandler(e))
+  create(produto: Produto): Observable<Produto> {
+    return this.http.post<Produto>(this.baseUrl + "/Adicionar/teste", produto).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
     )
   }
 
@@ -35,29 +35,29 @@ export class ProdutoService {
     return this.http.get<Produto[]>(this.baseUrl).pipe(
       map(obj => obj),
       catchError(e => this.errorHandler(e))
-      )
-    }
+    )
+  }
 
-    findById(id: string): Observable<Produto>{
+  findById(id: string): Observable<Produto> {
     const url = `${this.baseUrl}/${id}`
     return this.http.get<Produto>(url)
   }
 
-  update (produto: Produto): Observable<Produto> {
+  update(produto: Produto): Observable<Produto> {
     const url = `${this.baseUrl}/${produto.id}`
     return this.http.put<Produto>(url, produto).pipe(
       map(obj => obj),
       catchError(e => this.errorHandler(e))
-      )
-    }
+    )
+  }
 
-  delete(id: number): Observable<Produto>{
+  delete(id: number): Observable<Produto> {
     const url = `${this.baseUrl}/${id}`
     return this.http.delete<Produto>(url).pipe(
       map(obj => obj),
       catchError(e => this.errorHandler(e))
-      )
-    }
+    )
+  }
 
   errorHandler(e: any): Observable<any> {
     console.log(e)
